c412304cd1f870e07b61dfb78cf73cc7aef60ffc
void4002 <vineethreddypachika@gmail.com>
2023-05-21 19:54:12 +0530

    changed

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -5,8 +5,7 @@ export default function CTA() {
     <section className='py-12 md:py-24'>
       <div className='container px-4'>
         <div className='flex flex-col md:flex-row justify-center items-center gap-6 text-center md:text-start'>
-          <span className='flex-grow text-4xl md:text-5xl 2xl:text-6xl font-bold text-primary'>Dive into World of AI</span>
-          <button className='btn btn-secondary rounded-full sm:btn-lg'>Let's connect</button>
+          <span className='flex-grow text-4xl md:text-5xl 2xl:text-6xl font-bold text-primary text-center'>Dive into World of AI</span>
         </div>
       </div>
     </section>
