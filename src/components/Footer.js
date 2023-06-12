bb7c154f91aa0eba1de3f1709f6650f29ec59d64
void4002 <vineethreddypachika@gmail.com>
2023-06-12 11:26:54 +0530

    fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,10 @@ const Footer = () => {
           © 2023 VJCreatinAI. All rights reserved.
         </p>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-          <a className="text-gray-400" href='#/'>
+          <a className="text-gray-400" href='https://www.instagram.com/invites/contact/?i=njxrgbajq9hz&utm_content=rhz815f'>
           <i class="bi bi-instagram"></i>
           </a>
-          <a className="ml-3 text-gray-400" href='#/'>
+          <a className="ml-3 text-gray-400" href='https://www.linkedin.com/groups/12847041'>
           <i class="bi bi-linkedin"></i>
           </a>
         </span>
